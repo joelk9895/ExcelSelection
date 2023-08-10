7a7c926d6ab7d0d95e5b062186144e2d31f2a57b
Joel K George <joelkgeorge@Joels-MBP.local>
2023-08-10 11:52:13 +0530

    Final

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Model, { } from "./component/Knot"
 import localFont from 'next/font/local'
 
@@ -5,6 +6,9 @@ import localFont from 'next/font/local'
 const myFont = localFont({ src: './font.otf' })
 
 export default function Home() {
+  const handleclick = () => {
+    alert("Made it in a hurry, Hope You liked it!😌 \n" + "Check joelkgeorge.vercel.app or joelkgeorge.tech to see my portfolio")
+  }
   return (
     <main>
       <div className="filler3"></div>
@@ -13,7 +17,12 @@ export default function Home() {
       <div className="lander">
         <Model />
       </div>
-      <button className={myFont.className} id="Register">Register</button>
+      <button className={myFont.className} id="Register" onClick={handleclick}>Register</button>
+      <footer>
+        <p>Made by <a href="https://joelkgeorge.tech"
+          target="_blank" rel="noopener noreferrer"
+        >Joel K George</a></p>
+      </footer>
     </main>
   )
 }
